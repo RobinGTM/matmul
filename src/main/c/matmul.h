670997545ac59a848c65475514bfde62253e7227
RobinGTM <robingay67@gmail.com>
2025-06-24 11:45:16 -0400

    [WIP] host code

---
+++ src/main/c/matmul.h
@@ -0,0 +1,10 @@
+#ifndef __MATMUL_H__
+#define __MATMUL_H__
+
+#define M_HEIGHT 300
+#define M_WIDTH  300
+
+#define SOCK_H2C "/dev/xdma0_h2c_0"
+#define SOCK_C2H "/dev/xdma0_c2h_0"
+
+#endif /* __MATMUL_H__ */
