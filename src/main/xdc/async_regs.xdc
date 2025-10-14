RobinGTM/matmul

commit 2f541eaa810106b3a982d951086d34461596a058

[WIP] tweaks

scripts/TopLevel.tcl.in
@@ -69,14 +69,14 @@ if { [ file exist ../hw/vhdl ] } {
 }
 # Mandatory in non-project mode
 auto_detect_xpm
-read_xdc         [glob ${MATMUL_DIR}/src/main/xdc/*.xdc]
+read_xdc ${MATMUL_DIR}/src/main/xdc/matmul.xdc
 
 # Don't OOC XDMA
 set_property generate_synth_checkpoint false [get_files ${xdma_xci}]
 
 # Generate XDMA products
-set_property used_in_synthesis false [get_files matmul.xdc]
-set_property used_in_implementation false [get_files dont_touch.xdc]
+# set_property used_in_synthesis false [get_files matmul.xdc]
+# set_property used_in_implementation false [get_files dont_touch.xdc]
 # Generate IP products
 generate_target all [get_ips xdma]
 catch { config_ip_cache -export [get_ips -all xdma] }
@@ -109,8 +109,8 @@ if { ${OOC} == 1 } {
     } {
       puts "Found existing dcp, not re-synthesizing \"${ooc_module}\""
     } else {
-      set_property used_in_synthesis false [get_files matmul.xdc]
-      set_property used_in_implementation false [get_files dont_touch.xdc]
+      # set_property used_in_synthesis false [get_files matmul.xdc]
+      # set_property used_in_implementation false [get_files dont_touch.xdc]
       synth_design -part ${PART} -top ${ooc_module} -mode out_of_context
       # opt_design
       # Vivado does this
@@ -157,7 +157,7 @@ if { [ file exist ../hw/vhdl ] } {
 }
   auto_detect_xpm
   # Re-read constraints in new project
-  read_xdc [glob ${MATMUL_DIR}/src/main/xdc/*.xdc]
+  read_xdc ${MATMUL_DIR}/src/main/xdc/matmul.xdc
   # Re-read xdma IP
   read_ip ${xdma_xci}
 
@@ -183,8 +183,8 @@ if { [ file exist ../hw/vhdl ] } {
     read_checkpoint ${linked_dcp}
     open_checkpoint ${linked_dcp}
   } else {
-    set_property used_in_synthesis false [get_files matmul.xdc]
-    set_property used_in_implementation false [get_files dont_touch.xdc]
+    # set_property used_in_synthesis false [get_files matmul.xdc]
+    # set_property used_in_implementation false [get_files dont_touch.xdc]
     # Read netlists
     read_edif [glob ${EDF_DIR}/*/*]
     # Link
@@ -192,8 +192,8 @@ if { [ file exist ../hw/vhdl ] } {
     write_checkpoint -force ${linked_dcp}
   }
 } else {
-  set_property used_in_synthesis false [get_files matmul.xdc]
-  set_property used_in_implementation false [get_files dont_touch.xdc]
+  # set_property used_in_synthesis false [get_files matmul.xdc]
+  # set_property used_in_implementation false [get_files dont_touch.xdc]
   # Synth
   synth_design -top ${top_name} -part ${PART}
   write_checkpoint -force -noxdef ${DCP_DIR}/${top_name}.dcp
@@ -209,6 +209,7 @@ if { [ file exist ../hw/vhdl ] } {
 if { ${USER_SLR_CSTRS} == 1 } {
   set SLR_XDC ../hw/xdc/*slr_assignments*.xdc
   read_xdc [glob ${SLR_XDC}]
+  # set_property used_in_implementation true [get_files [glob ${SLR_XDC}]]
 }
 
 # Opt

src/main/xdc/matmul.xdc
@@ -33,36 +33,20 @@ set_clock_groups -asynchronous \
 set_clock_groups -asynchronous \
     -group [get_clocks _xdma_axi_aclk] \
     -group [get_clocks coreclk]
+# Declare false paths
+# set_false_path -from [get_clocks coreclk] -to [get_clocks _xdma_axi_aclk]
+# set_false_path -from [get_clocks _xdma_axi_aclk] -to [get_clocks coreclk]
 #
 #######################################
 # False paths for clock domain crossing
-# (should probably automate this)
-set_property ASYNC_REG true [get_cells axiW/axiLiteSlave/arMcpSrc/ackSyncFF1_reg]
-set_property ASYNC_REG true [get_cells axiW/axiLiteSlave/arMcpSrc/ackSyncFF2_reg]
-set_property ASYNC_REG true [get_cells axiW/axiLiteSlave/wrMcpSrc/ackSyncFF1_reg]
-set_property ASYNC_REG true [get_cells axiW/axiLiteSlave/wrMcpSrc/ackSyncFF2_reg]
-set_property ASYNC_REG true [get_cells axiW/iFifoWrPort/rCntMcpDst/loadSyncFF1_reg]
-set_property ASYNC_REG true [get_cells axiW/iFifoWrPort/rCntMcpDst/loadSyncFF2_reg]
-set_property ASYNC_REG true [get_cells axiW/iFifoWrPort/wCntMcpSrc/ackSyncFF1_reg]
-set_property ASYNC_REG true [get_cells axiW/iFifoWrPort/wCntMcpSrc/ackSyncFF2_reg]
-set_property ASYNC_REG true [get_cells axiW/oFifoRdPort/wCntMcpDst/loadSyncFF1_reg]
-set_property ASYNC_REG true [get_cells axiW/oFifoRdPort/wCntMcpDst/loadSyncFF2_reg]
-set_property ASYNC_REG true [get_cells axiW/oFifoRdPort/rCntMcpSrc/ackSyncFF1_reg]
-set_property ASYNC_REG true [get_cells axiW/oFifoRdPort/rCntMcpSrc/ackSyncFF2_reg]
-set_property ASYNC_REG true [get_cells axiW/axiLiteSlave/rdMcpDst/loadSyncFF1_reg]
-set_property ASYNC_REG true [get_cells axiW/axiLiteSlave/rdMcpDst/loadSyncFF2_reg]
-set_property ASYNC_REG true [get_cells core/oFifoWrPort/rCntMcpDst/loadSyncFF1_reg]
-set_property ASYNC_REG true [get_cells core/oFifoWrPort/rCntMcpDst/loadSyncFF2_reg]
-set_property ASYNC_REG true [get_cells core/iFifoRdPort/rCntMcpSrc/ackSyncFF1_reg]
-set_property ASYNC_REG true [get_cells core/iFifoRdPort/rCntMcpSrc/ackSyncFF2_reg]
-set_property ASYNC_REG true [get_cells core/mcpAdapter/wrMcpDst/loadSyncFF1_reg]
-set_property ASYNC_REG true [get_cells core/mcpAdapter/wrMcpDst/loadSyncFF2_reg]
-set_property ASYNC_REG true [get_cells core/oFifoWrPort/wCntMcpSrc/ackSyncFF1_reg]
-set_property ASYNC_REG true [get_cells core/oFifoWrPort/wCntMcpSrc/ackSyncFF2_reg]
-set_property ASYNC_REG true [get_cells core/iFifoRdPort/wCntMcpDst/loadSyncFF1_reg]
-set_property ASYNC_REG true [get_cells core/iFifoRdPort/wCntMcpDst/loadSyncFF2_reg]
-set_property ASYNC_REG true [get_cells core/mcpAdapter/arMcpDst/loadSyncFF1_reg]
-set_property ASYNC_REG true [get_cells core/mcpAdapter/arMcpDst/loadSyncFF2_reg]
+# (should probably automate this / make this more robust)
+set_property ASYNC_REG true [get_cells -hier ackSyncFF*]
+# set_false_path -to          [get_cells -hier ackSyncFF*]
+set_property ASYNC_REG true [get_cells -hier loadSyncFF*]
+# set_false_path -to          [get_cells -hier loadSyncFF*]
+set_property ASYNC_REG true [get_cells -hier sync_rstn*_reg]
+# set_false_path -to          [get_cells -hier sync_rstn*_reg]
+# set_false_path -from        [get_cells -hier sync_rstn*_reg]
 #
 #####################
 # Reset configuration
@@ -72,6 +56,7 @@ set_property PULLTYPE PULLUP [get_ports sys_rst_n]
 set_property IOSTANDARD POD12 [get_ports sys_rst_n]
 #
 set_property PACKAGE_PIN BD21 [get_ports sys_rst_n]
+# set_multicycle_path -from [get_ports sys_rst_n] -to [get_cells core/sync_rstn_REG_reg] 2
 # Voltage
 set_property CONFIG_VOLTAGE 1.8 [current_design]
 #

src/main/scala/utils/utils.scala
@@ -116,9 +116,10 @@ package object utils {
     val FLOAT = parseOpt("FLOAT", "f", "saf")
     require(FLOAT == "saf" || FLOAT == "hardfloat" || FLOAT == "flopoco",
       "FLOAT must be one of:\n" +
-      "- saf -- custom SAF implementation\n" +
+      "- saf       -- custom SAF implementation\n" +
       "- hardfloat -- Berkeley's hardfloat package\n" +
-      "- flopoco -- De Dinechin and Kumm's FloPoCo generator"
+      "- flopoco   -- De Dinechin and Kumm's FloPoCo generator\n" +
+      s"Got \"${FLOAT}\", which is invalid."
     )
     if(FLOAT == "flopoco") {
       require(MAC_DSP_PIPELINE_REGS == 0,

commit f083b04511db9e0f3b189918b3dfec50204dfb63

Fix timing constraint issues
introduced by 2f541eaa810106b3a982d951086d34461596a058 and FIFO integration rework

scripts/TopLevel.tcl.in
@@ -69,7 +69,7 @@ if { [ file exist ../hw/vhdl ] } {
 }
 # Mandatory in non-project mode
 auto_detect_xpm
-read_xdc         [glob ${MATMUL_DIR}/src/main/xdc/*.xdc]
+read_xdc [glob ${MATMUL_DIR}/src/main/xdc/*.xdc]
 
 # Don't OOC XDMA
 set_property generate_synth_checkpoint false [get_files ${xdma_xci}]

src/main/xdc/async_regs.xdc
@@ -1,0 +1,40 @@
+#######################################
+# ASYNC_REG properties for clock domain crossing
+# (should probably automate this)
+# Control MCPs
+set_property ASYNC_REG true [get_cells axiW/axiLiteSlave/arMcpSrc/ackSyncFF1_reg]
+set_property ASYNC_REG true [get_cells axiW/axiLiteSlave/arMcpSrc/ackSyncFF2_reg]
+set_property ASYNC_REG true [get_cells axiW/axiLiteSlave/wrMcpSrc/ackSyncFF1_reg]
+set_property ASYNC_REG true [get_cells axiW/axiLiteSlave/wrMcpSrc/ackSyncFF2_reg]
+set_property ASYNC_REG true [get_cells axiW/axiLiteSlave/rdMcpDst/loadSyncFF1_reg]
+set_property ASYNC_REG true [get_cells axiW/axiLiteSlave/rdMcpDst/loadSyncFF2_reg]
+set_property ASYNC_REG true [get_cells core/mcpAdapter/wrMcpDst/loadSyncFF1_reg]
+set_property ASYNC_REG true [get_cells core/mcpAdapter/wrMcpDst/loadSyncFF2_reg]
+set_property ASYNC_REG true [get_cells core/mcpAdapter/arMcpDst/loadSyncFF1_reg]
+set_property ASYNC_REG true [get_cells core/mcpAdapter/arMcpDst/loadSyncFF2_reg]
+# FIFO MCPs
+# Input FIFO write port (AXI side)
+set_property ASYNC_REG true [get_cells iFifoWrPort/rCntMcpDst/loadSyncFF1_reg]
+set_property ASYNC_REG true [get_cells iFifoWrPort/rCntMcpDst/loadSyncFF2_reg]
+set_property ASYNC_REG true [get_cells iFifoWrPort/rCntSyncReg]
+set_property ASYNC_REG true [get_cells iFifoWrPort/wCntMcpSrc/ackSyncFF1_reg]
+set_property ASYNC_REG true [get_cells iFifoWrPort/wCntMcpSrc/ackSyncFF2_reg]
+# Output FIFO read port (AXI side)
+set_property ASYNC_REG true [get_cells oFifoRdPort/wCntMcpDst/loadSyncFF1_reg]
+set_property ASYNC_REG true [get_cells oFifoRdPort/wCntMcpDst/loadSyncFF2_reg]
+set_property ASYNC_REG true [get_cells oFifoRdPort/wCntSyncReg]
+set_property ASYNC_REG true [get_cells oFifoRdPort/rCntMcpSrc/ackSyncFF1_reg]
+set_property ASYNC_REG true [get_cells oFifoRdPort/rCntMcpSrc/ackSyncFF2_reg]
+# Output FIFO write port (core side)
+set_property ASYNC_REG true [get_cells oFifoWrPort/wCntMcpSrc/ackSyncFF1_reg]
+set_property ASYNC_REG true [get_cells oFifoWrPort/wCntMcpSrc/ackSyncFF2_reg]
+set_property ASYNC_REG true [get_cells oFifoWrPort/rCntSyncReg]
+set_property ASYNC_REG true [get_cells oFifoWrPort/rCntMcpDst/loadSyncFF1_reg]
+set_property ASYNC_REG true [get_cells oFifoWrPort/rCntMcpDst/loadSyncFF2_reg]
+# Output FIFO read port (core side)
+set_property ASYNC_REG true [get_cells iFifoRdPort/wCntMcpDst/loadSyncFF1_reg]
+set_property ASYNC_REG true [get_cells iFifoRdPort/wCntMcpDst/loadSyncFF2_reg]
+set_property ASYNC_REG true [get_cells iFifoRdPort/wCntSyncReg]
+set_property ASYNC_REG true [get_cells iFifoRdPort/rCntMcpSrc/ackSyncFF1_reg]
+set_property ASYNC_REG true [get_cells iFifoRdPort/rCntMcpSrc/ackSyncFF2_reg]
+#######################################

src/main/xdc/matmul.xdc
@@ -34,36 +34,6 @@ set_clock_groups -asynchronous \
     -group [get_clocks _xdma_axi_aclk] \
     -group [get_clocks coreclk]
 #
-#######################################
-# False paths for clock domain crossing
-# (should probably automate this)
-set_property ASYNC_REG true [get_cells axiW/axiLiteSlave/arMcpSrc/ackSyncFF1_reg]
-set_property ASYNC_REG true [get_cells axiW/axiLiteSlave/arMcpSrc/ackSyncFF2_reg]
-set_property ASYNC_REG true [get_cells axiW/axiLiteSlave/wrMcpSrc/ackSyncFF1_reg]
-set_property ASYNC_REG true [get_cells axiW/axiLiteSlave/wrMcpSrc/ackSyncFF2_reg]
-set_property ASYNC_REG true [get_cells axiW/iFifoWrPort/rCntMcpDst/loadSyncFF1_reg]
-set_property ASYNC_REG true [get_cells axiW/iFifoWrPort/rCntMcpDst/loadSyncFF2_reg]
-set_property ASYNC_REG true [get_cells axiW/iFifoWrPort/wCntMcpSrc/ackSyncFF1_reg]
-set_property ASYNC_REG true [get_cells axiW/iFifoWrPort/wCntMcpSrc/ackSyncFF2_reg]
-set_property ASYNC_REG true [get_cells axiW/oFifoRdPort/wCntMcpDst/loadSyncFF1_reg]
-set_property ASYNC_REG true [get_cells axiW/oFifoRdPort/wCntMcpDst/loadSyncFF2_reg]
-set_property ASYNC_REG true [get_cells axiW/oFifoRdPort/rCntMcpSrc/ackSyncFF1_reg]
-set_property ASYNC_REG true [get_cells axiW/oFifoRdPort/rCntMcpSrc/ackSyncFF2_reg]
-set_property ASYNC_REG true [get_cells axiW/axiLiteSlave/rdMcpDst/loadSyncFF1_reg]
-set_property ASYNC_REG true [get_cells axiW/axiLiteSlave/rdMcpDst/loadSyncFF2_reg]
-set_property ASYNC_REG true [get_cells core/oFifoWrPort/rCntMcpDst/loadSyncFF1_reg]
-set_property ASYNC_REG true [get_cells core/oFifoWrPort/rCntMcpDst/loadSyncFF2_reg]
-set_property ASYNC_REG true [get_cells core/iFifoRdPort/rCntMcpSrc/ackSyncFF1_reg]
-set_property ASYNC_REG true [get_cells core/iFifoRdPort/rCntMcpSrc/ackSyncFF2_reg]
-set_property ASYNC_REG true [get_cells core/mcpAdapter/wrMcpDst/loadSyncFF1_reg]
-set_property ASYNC_REG true [get_cells core/mcpAdapter/wrMcpDst/loadSyncFF2_reg]
-set_property ASYNC_REG true [get_cells core/oFifoWrPort/wCntMcpSrc/ackSyncFF1_reg]
-set_property ASYNC_REG true [get_cells core/oFifoWrPort/wCntMcpSrc/ackSyncFF2_reg]
-set_property ASYNC_REG true [get_cells core/iFifoRdPort/wCntMcpDst/loadSyncFF1_reg]
-set_property ASYNC_REG true [get_cells core/iFifoRdPort/wCntMcpDst/loadSyncFF2_reg]
-set_property ASYNC_REG true [get_cells core/mcpAdapter/arMcpDst/loadSyncFF1_reg]
-set_property ASYNC_REG true [get_cells core/mcpAdapter/arMcpDst/loadSyncFF2_reg]
-#
 #####################
 # Reset configuration
 set_false_path -from [get_ports sys_rst_n]